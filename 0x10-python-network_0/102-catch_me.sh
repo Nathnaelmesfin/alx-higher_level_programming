3e4411faff68faff039cc9fabbceb13593f8ec6e
Nathnael Mesfin Kifle <nathnaelmesfin77@gmail.com>
2024-01-25 23:12:19 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "You got me!"
+# causes the server to respond with a message
+curl -sLX PUT -H "origin:You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
